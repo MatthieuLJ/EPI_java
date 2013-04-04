054686e303985ecbedcfe1134166b811ea1fc0df
Matthieu Jeanson <matthieu.jeanson@gmail.com>
2013-04-04 16:23:38 -0700

    Return the cycle start in problem 4.2, both methods

--- src/com/matthieu/epi/CheckingCyclicity.java
+++ src/com/matthieu/epi/CheckingCyclicity.java
@@ -4,18 +4,18 @@ import java.util.HashSet;
 
 public class CheckingCyclicity implements Solution {
 
-    public <T> boolean hasCycle(LinkedList<T> list) {
+    public <T> LinkedList<T> hasCycle(LinkedList<T> list) {
         HashSet<LinkedList<T>> hash = new HashSet<LinkedList<T>>();
         while (list != null) {
             if (hash.contains(list))
-                return true;
+                return list;
             hash.add(list);
             list = list.next;
         }
-        return false;
+        return null;
     }
 
-    public <T> boolean hasCycle2(LinkedList<T> list) {
+    public <T> LinkedList<T> hasCycle2(LinkedList<T> list) {
         LinkedList<T> slow=list, fast=list;
         do {
             slow = slow.next;
@@ -24,7 +24,27 @@ public class CheckingCyclicity implements Solution {
                 fast = fast.next;
         } while ((fast != slow) && (fast != null));
 
-        return (fast != null);
+        if (fast == null)
+            return null;
+
+        int cycle_length=0;
+        do {
+            fast = fast.next;
+            cycle_length++;
+        } while (slow != fast);
+
+        fast = list;
+        slow = list;
+        while (cycle_length > 0) {
+            fast = fast.next;
+            cycle_length--;
+        }
+
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 
     private LinkedList<Integer> createLinkedList(int size) {
@@ -68,11 +88,11 @@ public class CheckingCyclicity implements Solution {
         LinkedList<Integer> no_cycle = createLinkedList(15);
         LinkedList<Integer> with_cycle = addCycle(createLinkedList(20), 10);
 
-        System.out.println("First list "+(hasCycle(no_cycle)?"has":"does not have")+" a cycle");
-        System.out.println("Second list "+(hasCycle(with_cycle)?"has":"does not have")+" a cycle");
+        System.out.println("First list "+(hasCycle(no_cycle)!=null?"has":"does not have")+" a cycle");
+        System.out.println("Second list "+(hasCycle(with_cycle)!=null?"has":"does not have")+" a cycle");
 
-        System.out.println("First list "+(hasCycle2(no_cycle)?"has":"does not have")+" a cycle");
-        System.out.println("Second list "+(hasCycle2(with_cycle)?"has":"does not have")+" a cycle");
+        System.out.println("First list "+(hasCycle2(no_cycle)!=null?"has":"does not have")+" a cycle");
+        System.out.println("Second list "+(hasCycle2(with_cycle)!=null?"has":"does not have")+" a cycle");
 
     }
 }
