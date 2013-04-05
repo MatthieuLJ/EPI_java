c6f4326cca9e5f25c6b6de473e5d1fe18ff6a01e
Matthieu Jeanson <matthieu.jeanson@gmail.com>
2013-04-05 14:44:23 -0700

    Add problem 11.7 (and binary tree structure)

---
+++ src/com/matthieu/epi/BSTFromSortedArray.java
@@ -0,0 +1,35 @@
+package com.matthieu.epi;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BSTFromSortedArray implements Solution {
+    public <T extends Comparable<T>> BinaryTree<T> buildFromArray(T t[]) {
+        if (t.length==0)
+            return null;
+
+        BinaryTree<T> res = new BinaryTree<T>();
+        if (t.length==1) {
+            res.key = t[0];
+            return res;
+        }
+        int mid_point = t.length/2;
+        res.key=t[mid_point];
+        res.left = buildFromArray(Arrays.copyOfRange(t, 0, mid_point-1));
+        res.right = buildFromArray(Arrays.copyOfRange(t, mid_point+1, t.length));
+
+        return res;
+    }
+
+    @Override
+    public void solveProblem() {
+        Integer h[] = new Integer[10];
+        for (int i=0; i<h.length; i++) {
+            h[i] = (int) (Math.random()*30);
+        }
+        Arrays.sort(h);
+
+        System.out.println("BST generated from array: "+Arrays.toString(h));
+        System.out.println("is : "+buildFromArray(h));
+    }
+}
---
+++ src/com/matthieu/epi/BinaryTree.java
@@ -0,0 +1,37 @@
+package com.matthieu.epi;
+
+public class BinaryTree<T> {
+    public BinaryTree<T> left=null;
+    public BinaryTree<T> right=null;
+    public T key;
+
+    public boolean isLeaf() {
+        return (left==null) && (right==null);
+    }
+
+    public static BinaryTree<Integer> buildRandomTree(int h) {
+        BinaryTree<Integer> node = new BinaryTree<Integer>();
+        node.key = (int) (Math.random() * 50);
+        if (h > 0) {
+            node.left = buildRandomTree(h-1);
+            node.right = buildRandomTree(h-1);
+        }
+        return node;
+    }
+
+    public int getHeight() {
+        if ((left==null) && (right==null))
+            return 0;
+        if (left == null)
+            return 1+right.getHeight();
+        if (right == null)
+            return 1+left.getHeight();
+
+        return 1+Math.max(left.getHeight(), right.getHeight());
+    }
+
+    public String toString() {
+        return key.toString() + (left!=null || right!=null ? " [":"") + (left!=null?left.toString():"") +
+                (left!=null && right!=null ? " : ":"") + (right!=null?right.toString():"") + (left!=null || right!=null ? " ]":"");
+    }
+}
